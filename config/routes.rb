46d1dbf349d6613b550c724d82723a3be36ee252
amasalov <delsol@bk.ru>
2018-12-27 22:32:51 +0300

    add train

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :railway_stations
+  resources :trains
   get 'welcome/index'
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
