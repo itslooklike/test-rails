7e01c3e130a09d21db4901b184f3918c3106545d
amasalov <delsol@bk.ru>
2019-01-07 02:25:28 +0300

    improve

--- app/models/route.rb
+++ app/models/route.rb
@@ -1,3 +1,4 @@
 class Route < ApplicationRecord
-  has_and_belongs_to_many :railway_stations
+  has_many :railway_stations_routes
+  has_many :railway_stations, through: :railway_stations_routes
 end
